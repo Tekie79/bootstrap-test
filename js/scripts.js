8c2c797c6ceba6def17cdc6cfab4fc1cf369f04f
Tekie79 <76453162+Tekie79@users.noreply.github.com>
2021-02-06 12:25:04 -0500

    Week-4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000, pause: "false" } );
-    $("#carouselButton").click(function(){
-       if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-           $(".carousel").carousel("pause");
-           $("#carouselButton").children("i").removeClass("fa-pause");
-           $("#carouselButton").children("i").addClass("fa-play");
-       } else {
-        $(".carousel").carousel("cycle");
-        $("#carouselButton").children("i").removeClass("fa-play");
-           $("#carouselButton").children("i").addClass("fa-pause");
-       }
-    });
+$(function () {
+  $(".carousel").carousel({ interval: 2000, pause: "false" });
+  $("#carouselButton").on("click", function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
+});
+
+// Reserve campsite and login modals click events
+
+$(function () {
+  $("#reserveButton").on("click", function () {
+    $("#reserveModal").modal("show");
+  });
+  $("#loginButton").on("click", function () {
+    $("#loginModal").modal("show");
+  });
 });
